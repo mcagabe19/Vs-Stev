b8ed807da7d58a5ca7e802c14de07d31bfef304f
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-07-04 22:49:59 +0300

    no plotsh shit

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -905,14 +905,6 @@ case 'flat': //Super Duper Flat Stage
 		add(iconP2);
 		reloadHealthBarColors();
         
-        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Plotsh "), 24);
-        creditTxt.scrollFactor.set();
-        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-        creditTxt.borderColor = FlxColor.BLACK;
-        creditTxt.borderSize = 3;
-        creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
-        add(creditTxt);
-        
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
